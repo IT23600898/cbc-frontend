34743e65bc7d67e1bd2bf6ff148dcbbdbd54a497
U L R Kavindya <it23600898@my.sliit.lk>
2025-08-02 20:29:53 +0530

    Create admin products page 🛒🛒

---
+++ pages/admin/adminProductPage.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+export default function AdminProductPage(){
+
+    getProducts()
+    
+    return(
+        <div>
+            <h1>Admin Products Page</h1>
+        </div>
+    )
+}
+
+async function getProducts(){
+    const res = await axios.get("http://localhost:5000/api/products")
+    console.log(res)
+}
